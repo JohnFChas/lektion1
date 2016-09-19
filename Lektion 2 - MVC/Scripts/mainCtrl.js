36c170188721104a2310144d4c3c7fc1f02a22cd
John Fristedt <john.fristedt@chas.se>
2016-09-19 11:52:06 +0200

    Initial Commit

---
+++ Lektion 2 - MVC/Scripts/mainCtrl.js
@@ -0,0 +1,37 @@
+﻿/// <reference path="../angular.js" />
+/// <reference path="../Apps/app.js" />
+
+app.controller("mainCtrl", [
+    "$scope",
+    function ($scope) { // <-- This function is where the magic happens
+        $scope.title = "My webshop";
+        $scope.showList = true;
+
+        $scope.products = [
+            {
+                name: "Product 1",
+                description: "Product 1 description"
+            },
+            {
+                name: "Product 2",
+                description: "Product 2 description"
+            },
+            {
+                name: "Product 3",
+                description: "Product 3 description"
+            },
+            {
+                name: "Product 4",
+                description: "Product 4 description"
+            },
+            {
+                name: "Product 5",
+                description: "Product 5 description"
+            },
+        ];
+
+        $scope.toggleList = function () {
+            $scope.showList = !$scope.showList;
+        };
+    }
+]);
