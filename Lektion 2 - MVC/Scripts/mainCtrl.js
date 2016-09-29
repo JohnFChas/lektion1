c6f549f512eda40a18d4989ab793d4d4e0b7930b
John Fristedt <john.fristedt@chas.se>
2016-09-29 10:13:31 +0200

    Added a form for adding new products

--- Lektion 2 - MVC/Scripts/mainCtrl.js
+++ Lektion 2 - MVC/Scripts/mainCtrl.js
@@ -6,6 +6,10 @@ app.controller("mainCtrl", [
     function ($scope) { // <-- This function is where the magic happens
         $scope.title = "My webshop";
         $scope.showList = true;
+        $scope.newProduct = {
+            name: "",
+            description: ""
+        };
 
         $scope.products = [
             {
@@ -28,7 +32,15 @@ app.controller("mainCtrl", [
                 name: "Product 5",
                 description: "Product 5 description"
             },
+            {
+                name: "Product 6",
+                description: "Product 6 description"
+            },
         ];
+
+        $scope.addProduct = function () {
+            $scope.products.push($scope.newProduct);
+        }
 
         $scope.toggleList = function () {
             $scope.showList = !$scope.showList;
